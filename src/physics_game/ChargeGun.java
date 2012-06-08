d59121e05c845989ce03aa20903857c8d92c2741
Kevin Jin <goldstar.kevin@gmail.com>
2012-06-08 23:06:19 +0000

    * RayGun -> ChargeGun

--- src/physics_game/ChargeGun.java
+++ src/physics_game/ChargeGun.java
@@ -1,25 +1,28 @@
 package physics_game;
 
-public class ChargeGun {
-	
-	private final static double MAX_ANGLE = Math.PI*2;
+import java.awt.image.BufferedImage;
+
+public class ChargeGun extends StationaryEntity {
+	private final static double MAX_ANGLE = Math.PI * 2;
 	private final static double MIN_ANGLE = 0;
-	
+
 	private double mult;
 	private KeyBindings binding;
 	private boolean positive;
 	private boolean actionHeld;
 	private int totalPoints;
-	
-	public ChargeGun(boolean p1){
+
+	public ChargeGun(boolean p1) {
 		binding = new KeyBindings(p1);
 		mult = (p1) ? 3 : -3;
 	}
+
 	public KeyBindings getKeyBindings() {
 		return binding;
 	}
+
 	public void update(int change, boolean action, double tDelta) {
-		double d = 0/*body.getRotation()*/;
+		double d = 0/* body.getRotation() */;
 		d += Math.signum(change) * tDelta * mult;
 		if (d < MIN_ANGLE)
 			d = MIN_ANGLE;
@@ -29,9 +32,11 @@ public class ChargeGun {
 			positive = !positive;
 		actionHeld = action;
 	}
-	public void setPosition(Position p){
-		
+
+	public void setPosition(Position p) {
+
 	}
+
 	public void addPoints(int add) {
 		totalPoints += add;
 	}
@@ -40,4 +45,13 @@ public class ChargeGun {
 		return totalPoints;
 	}
 
+	@Override
+	public BufferedImage getTexture() {
+		return TextureCache.getTexture((positive ? "pos" : "neg") + "gun");
+	}
+
+	@Override
+	public boolean transformAboutCenter() {
+		return false;
+	}
 }
